e3e01036cc30782e7399b5355a607fea1c64b2d2
Piotr Domian <domianpiotr111@gmail.com>
2021-04-28 18:36:03 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,36 +1,97 @@
+var wallet = 0;
+var incomes = 0;
+var expenses = 0;
+
 document.addEventListener("DOMContentLoaded", (e) => {
-    const incomes = document.querySelector("#incomes");
-    const buttons = document.querySelector("#buttons")
-    const testForm1 = document.querySelector("#testForm1");
+    const incomesList = document.querySelector("#incomesList");
+    const incomesSum = document.querySelector("#incomes")
+    const expensesList = document.querySelector("#expensesList");
+    const expensesSum = document.querySelector("#expenses")
+    const btnRemove = document.querySelector("#btnRemove")
+    const testFormIncomeMain = document.querySelector("#testFormIncomeMain");
     const testFormIncome = document.querySelector("#testFormIncome");
     const testFormValue = document.querySelector("#testFormValue");
+    const howSpendMoney = document.querySelector("#howSpend")
+    
+    incomesSum.innerHTML = "Suma przychodów: " + incomes + " zł";
+    expensesSum.innerHTML = "Suma wydatków: " + expenses + " zł";
+    howSpendMoney.innerHTML = "Można wydać: " +  wallet + " zł";
 
-    testForm1.addEventListener("submit", (e) => {
+
+    // TODO  create function which upadte wallet incomes and expenses
+
+    testFormIncomeMain.addEventListener("submit", (e) => {
         e.preventDefault();
 
         let income = {
-            wydatek: testFormIncome.value,
-            kwota: testFormValue.value,
+            przychod: testFormIncome.value,
+            kwotaIncome: testFormValue.value,
         };
         
         const li = document.createElement("li");
-        li.innerText = `${income.wydatek} - ${income.kwota} zł`;
+        li.innerText = `${income.przychod} - ${income.kwotaIncome} zł`;
         console.log(li);
-        li.dataset.wydatek = income.wydatek;
-        li.dataset.id = 1;
-        incomes.appendChild(li);
+        // li.dataset.wydatek = income.wydatek;
+        // li.dataset.id = 1;
+        incomesList.appendChild(li);
 
         const btnEdit = document.createElement("button");
         btnEdit.innerText = "Edytuj";
         btnEdit.id = "btnEdit";
-        incomes.appendChild(btnEdit);
+        li.appendChild(btnEdit);
+
+        const btnRemove = document.createElement("button");
+        btnRemove.innerText = "Usuń";
+        btnRemove.dataset.id = Math.random() * 10000;
+        li.appendChild(btnRemove);
+        
+        // const sum = document.createElement("h1")
+        // sum1.appendChild(sum);
+
+        incomes += parseFloat(income.kwotaIncome);
+        incomesSum.innerHTML = "Suma przychodów: " + incomes + " zł";
+        wallet = incomes - expenses;
+        howSpendMoney.innerHTML = "Można wydać: " + wallet + " zł";
+    })
+
+    testFormExpenseMain.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        let expense = {
+            wydatek: testFormExpenses.value,
+            kwotaExpense: testFormValueExpense.value,
+        };
+        
+        const li = document.createElement("li");
+        li.innerText = `${expense.wydatek} - ${expense.kwotaExpense} zł`;
+        console.log(li);
+        // li.dataset.wydatek = income.wydatek;
+        // li.dataset.id = 1;
+        expensesList.appendChild(li);
+
+        const btnEdit = document.createElement("button");
+        btnEdit.innerText = "Edytuj";
+        btnEdit.id = "btnEdit";
+        li.appendChild(btnEdit);
 
         const btnRemove = document.createElement("button");
         btnRemove.innerText = "Usuń";
         btnRemove.id = "btnRemove";
-        incomes.appendChild(btnRemove);
+        li.appendChild(btnRemove);
         
-        const sum = document.createElement("h1")
-        sum1.appendChild(sum);
+        // const sum = document.createElement("h1")
+        // sum1.appendChild(sum);
+
+        expenses += parseFloat(expense.kwotaExpense);
+        expensesSum.innerHTML = "Suma wydatków: " + expenses +" zł";
+
+        wallet = incomes - expenses;
+        howSpendMoney.innerHTML = "Można wydać: " + wallet + " zł";
     })
+
+
+    btnRemove.addEventListener("click", (e) => {
+        console.log("usunieto");
+        li.remove();
+    });
 });
